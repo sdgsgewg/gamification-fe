588763734aaac57eeb314d62cc3788814558fcc2
Jessen <jessen123ptk@gmail.com>
2025-11-13 16:36:50 +0800

    fix page with dynamic route

--- app/(root)/activities/page.tsx
+++ app/(root)/activities/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import React, { useEffect, useRef, useState } from "react";
 import { useSearchParams } from "next/navigation";
 import { FilterModal } from "@/app/components/modals/FilterModal";
--- app/dashboard/teacher/submissions/page.tsx
+++ app/dashboard/teacher/submissions/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import React, { useEffect, useState } from "react";
 import Button from "@/app/components/shared/Button";
 import {

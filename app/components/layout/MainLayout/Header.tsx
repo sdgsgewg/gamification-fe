488c865dbf0f7ab33f9efa20945a7cc767adf4f9
Vito Satriana <vitosatriana2004@gmail.com>
2025-11-17 10:57:49 +0700

    change some texts to english

--- app/components/layout/MainLayout/Header.tsx
+++ app/components/layout/MainLayout/Header.tsx
@@ -104,13 +104,13 @@ const AuthActionButtons = () => {
         onClick={() => router.push(ROUTES.AUTH.LOGIN)}
         className="bg-tertiary text-tx-primary-accent font-bold rounded-2xl px-6 py-2 hover:bg-tertiary-hover transition cursor-pointer"
       >
-        Masuk
+        Login
       </button>
       <button
         onClick={() => router.push(ROUTES.AUTH.REGISTER)}
         className="bg-primary text-white font-bold rounded-2xl px-6 py-2 hover:bg-primary-hover transition cursor-pointer"
       >
-        Daftar
+        Register
       </button>
       <ThemeSwitcher />
     </div>
